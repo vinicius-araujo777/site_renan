6b264e9f1cf1657fcaa92190402719d5faf88f24
vinicius-araujo777 <vinicius.araujo60@aluno.ce.gov.br>
2025-11-16 16:20:53 -0300

    terminando validação de email,senha e possíveis erros

--- scripts.js
+++ scripts.js
@@ -2,6 +2,25 @@ const form = document.getElementById('form');
 const campos = document.querySelectorAll('.required');
 const smalls = document.querySelectorAll('.msgError');
 const smallsValidade = document.querySelectorAll('.msgValidade')
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+form.addEventListener('submit', (event) => {
+    nameValidate();
+    telefoneValidade();
+    emailValidade()
+    senhaValidade();
+    compareSenha();
+    const erros = document.querySelectorAll('.msgError[style*="block"]');
+    if(erros.length > 0)
+    {
+         event.preventDefault();
+    }
+    else
+    {
+
+    }
+});
+
 
 function setError(index) {
     campos[index].style.border = '2px solid red';
@@ -35,5 +54,41 @@ function telefoneValidade() {
     else
     {
         removerError(1);
+    }
+}
+
+
+function emailValidade(){
+    if(regex.test(campos[2].value))
+    {
+        removerError(2)
+    }
+    else
+    {
+        setError(2)
+    }
+}
+
+function senhaValidade(){
+    if(campos[3].value.length < 6)
+    {
+        setError(3);
+    }
+    else
+    {
+        removerError(3);
+        compareSenha();
+    }
+}
+
+
+function compareSenha(){
+    if(campos[3].value == campos[4].value && campos[4].value.length >= 6)
+    {
+        removerError(4);
+    }
+    else
+    {
+        setError(4);
     }
 }
